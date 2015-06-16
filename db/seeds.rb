e723c392ccaf3aada805202e17fe7ff0744f7d00
Cara Hill <chilly23@gmail.com>
2015-06-17 11:17:03 +1200

    Added faker seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.destroy_all
+
+10.times do Product.create(
+  name: Faker::Commerce.product_name,
+  image: Faker::Avatar.image,
+  description: Faker::Lorem.paragraph,
+  price: Faker::Commerce.price,
+  category: Faker::Lorem.word
+  )
+end
